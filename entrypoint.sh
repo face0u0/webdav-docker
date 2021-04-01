8f01d98182cc98fb2a14beba58b790c571a31715
face0u0 <me@face0u0.net>
2021-04-01 02:00:28 +0000

    set user uid/gid

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 if [ -n "$USERNAME" ]; then
 	htpasswd -bc /opt/nginx/conf/htpasswd $USERNAME $PASSWORD	
 	echo "add new user!"
+	addgroup -g 1001 davuser
+	adduser davuser -u 1001 -G davuser -D
 	exit 0
 fi
 
