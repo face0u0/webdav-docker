4cfb19811e29025e491338c6d3b4216ec4505cec
face0u0 <me@face0u0.net>
2021-05-19 16:21:37 +0000

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 if [ -n "$USERNAME" ]; then
 	htpasswd -bc /opt/nginx/conf/htpasswd $USERNAME $PASSWORD	
 	echo "add new user!"
-	addgroup -g 1001 davuser
-	adduser davuser -u 1001 -G davuser -D
+	addgroup -g $GID davuser
+	adduser davuser -u $UID -G davuser -D
 	exit 0
 fi
 
-echo "username not found"
+echo "environment variables not found"
 exit 1
